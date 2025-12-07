0588ec16531a9f36fe47fd8768723e3a5d24c60a
SzilardKarsai <karsaiszilard@gmail.com>
2025-12-07 22:42:39 +0100

    Fragment-ek hozzaadasa + navigacio

---
+++ app/src/main/java/hu/nje/openlibrarycatalog/ui/HomeFragment.java
@@ -0,0 +1,44 @@
+package hu.nje.openlibrarycatalog.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import androidx.navigation.Navigation;
+
+import org.jspecify.annotations.NonNull;
+
+import hu.nje.openlibrarycatalog.databinding.FragmentHomeBinding;
+
+import hu.nje.openlibrarycatalog.R;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        binding.btnStartSearch.setOnClickListener(v ->
+            Navigation.findNavController(v).navigate(R.id.searchFragment)
+        );
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
